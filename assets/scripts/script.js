e36e9fd0d8687461f30f61c055b6925e46bb1630
Nelgar20 <black_w@hotmail.it>
2026-05-14 19:58:36 +0100

    create js file and question array

---
+++ assets/scripts/script.js
@@ -0,0 +1,80 @@
+// Questions and answers
+const QnA = [
+    {
+        question: "What is the occasion?",
+        answers: [
+            {
+                text: "Learn cooking techniques",
+                score: ""
+            },
+            {
+                text: "Romantic dinner",
+                score: ""
+            },
+            {
+                text: "Family gathering",
+                score: ""
+            },
+            {
+                text: "Special event",
+                score: ""
+            }
+        ]
+    },
+    {
+        question: "How many guest will be attending?",
+        answers: [
+            {
+                text: "0",
+                score: ""
+            },
+            {
+                text: "2",
+                score: ""
+            },
+            {
+                text: "3-6",
+                score: ""
+            },
+            {
+                text: "7+",
+                score: ""
+            }
+        ]
+    },
+    {
+        question: "What kind of experience are you looking for?",
+        answers: [
+            {
+                text: "An interactive cooking lesson",
+                score: ""
+            },
+            {
+                text: "A relaxed dining experience",
+                score: ""
+            },
+            {
+                text: "Host a memorable event",
+                score: ""
+            }
+        ]
+    },
+    {
+        question: "What atmosphere would you like to create?",
+        answers: [
+            {
+                text: "Warm and casual",
+                score: ""
+            },
+            {
+                text: "Elegant and intimate",
+                score: ""
+            },
+            {
+                text: "Lively and celebratory",
+                score: ""
+            }
+        ]
+    }
+];
+
